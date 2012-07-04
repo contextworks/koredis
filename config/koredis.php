fcec0ee97061b9837af1e84cef94eb2c24100455
JeremyWei <shuimuqingshu@gmail.com>
2012-07-04 09:22:45 +0800

    edit config file comments

--- config/koredis.php
+++ config/koredis.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'host' => '127.0.0.1', // ip address or Unix socket(for example: /tmp/redis.sock), 127.0.0.1 default
-	'port' => 6379,        // port, 6379 default
-	'timeout' => 1,        // timeout, 1s default
-	'pconnect' => TRUE,    // whether using pconnect, TRUE default
+	'host' => '127.0.0.1', // ip address or Unix socket(for example: /tmp/redis.sock), default is 127.0.0.1
+	'port' => 6379,        // port, default is 6379
+	'timeout' => 1,        // timeout, default is 1(s)
+	'pconnect' => TRUE,    // whether using pconnect, default is TRUE
 );
